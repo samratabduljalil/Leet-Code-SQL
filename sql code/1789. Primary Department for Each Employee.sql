714e280d448412564ef957c264ce4fbcfc666a0a
Samrat Abdul Jalil <95965521+samratabduljalil@users.noreply.github.com>
2024-12-02 22:18:59 +0600

    Add files via upload

---
+++ sql code/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select employee_id, department_id
+from employee
+where primary_flag='Y'
+or employee_id not in (select employee_id from employee where primary_flag = "Y")
